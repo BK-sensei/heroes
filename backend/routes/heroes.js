cf15d4444d9c7240b680e500443724c901e934bc
becca <rebecca.kaci@gmail.com>
2021-12-09 18:28:43 +0100

    ajout du middleware qui vérifie que le hero n'existe pas et de la condition si l'héro ajouté est déjà présent dans la liste, alors on renvoit un message d'erreur sinon, le nouvel héro est ajouté à la liste

--- backend/routes/heroes.js
+++ backend/routes/heroes.js
@@ -3,19 +3,19 @@ const app = express()
 
 let superHeroes = require("../superheroes.json")
 
-// route heroes qui récupère tous les héros
+//--- route heroes qui récupère tous les héros
 app.get("/", (req, res) => {
     res.json(superHeroes)
 })
 
-// route qui récupère un super héros
+//--- route qui récupère un super héros
 app.get("/:slug", (req, res) => {
     const { slug } = req.params
     const hero = superHeroes.find(hero => hero.slug === slug )
     res.json(power)
 })
 
-// route qui récupère les pouvoirs d'un super héros
+//--- route qui récupère les pouvoirs d'un super héros
 app.get("/:slug/powers", (req, res) => {
     const { slug } = req.params
     const power = superHeroes.find(power => power.slug === slug )
@@ -23,18 +23,27 @@ app.get("/:slug/powers", (req, res) => {
     res.json(powers)
 })
 
-// route qui ajoute un super héros
+//--- route qui ajoute un super héros
 app.post("/", (req, res) => {
-    const hero = {
-        id: superHeroes.length + 1,
-        ...req.body
+    // middleware qui vérifie que le hero n'existe pas
+    const existingHero = superHeroes.find(hero => hero.slug === req.body.slug)
+
+    // condition si l'héro ajouté est déjà présent dans la liste, alors on renvoit un message d'erreur sinon, le nouvel héro est ajouté à la liste
+    if (existingHero){
+        res.status(409).send("This hero is already in the list")
+    } else {
+        const hero = {
+            id: superHeroes.length + 1,
+            ...req.body
+        }
+        superHeroes = [...superHeroes, hero]
     }
-    superHeroes = [...superHeroes, hero]
+
     res.json(superHeroes)
 })
 
-// route qui ajoute un pouvoir à un super héros
-app.put("/:slug/powers", (req,res) => {
+//--- route qui ajoute un pouvoir à un super héros
+app.put("/:slug/powers", (req, res) => {
     const { slug } = req.params
     const heroPower = superHeroes.find(power => power.slug === slug)
     const superpower = req.body.power // power = clé 'power' du fichier json
