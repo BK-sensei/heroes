77d7c770ba4e77755fb638b00372fe09f6af4853
becca <rebecca.kaci@gmail.com>
2021-12-14 12:39:23 +0100

    correction de petits éléments

--- backend/routes/heroes.js
+++ backend/routes/heroes.js
@@ -7,8 +7,8 @@ let superHeroes = require("../superheroes.json")
 
 //--- middleware qui vérifie que le hero n'existe pas
 const existingHero = (req, res, next) => {
-    const { slug } = req.body
-    const hero = superHeroes.find(hero => hero.slug === slug)
+    const { name } = req.body
+    const hero = superHeroes.find(hero => hero.name === name)
 
     if (hero) {
         res.status(409).send("This hero is already in the list")
@@ -17,15 +17,26 @@ const existingHero = (req, res, next) => {
     }
 }
 
+//--- middleware qui verifie qu'un hero existe avant de faire la suite
+const successIfExists = (req, res, next) => {
+    const { slug } = req.params
+    const hero = superHeroes.find(hero => hero.slug === slug)
+  
+    if (hero) {
+      next()
+    } else {
+      res.send(404).send("Hero not found")
+    }
+}
+
 //--- middleware qui valide les info arrivant dans le body de la requête sont les mêmes que le format de base
 const validateHero = (req, res, next) => {
-    const element = superHeroes[0]
-    const heroKeys = Object.keys(element)
-    const heroBody = Object.keys(req.body)
-    const invalidKey = heroBody.find(key => !heroKeys.icludes(key))
+    const allowedKeys = Object.keys(superHeroes[0])
+    const bodyKeys = Object.keys(req.body)
+    const invalidKey = bodyKeys.find(key => !allowedKeys.icludes(key))
 
     if (invalidKey){
-        res.status(409).send(`The key ${invalidKey} does not exist`)
+        res.status(400).send("This request is invalide")
     } else {
         next()
     }
@@ -40,74 +51,96 @@ app.get("/", (req, res) => {
 })
 
 //--- route qui récupère un super héros
-app.get("/:slug", existingHero, (req, res) => {
+app.get("/:slug", successIfExists, (req, res) => {
     const { slug } = req.params
     const hero = superHeroes.find(hero => hero.slug === slug)
     res.json(hero)
 })
 
 //--- route qui récupère les pouvoirs d'un super héros
-app.get("/:slug/power", (req, res) => {
+app.get("/:slug/power", successIfExists, (req, res) => {
     const { slug } = req.params
-    const power = superHeroes.find(power => power.slug === slug )
-    const powers = power.power
+    const hero = superHeroes.find(power => power.slug === slug )
+    const powers = hero.power
     res.json(powers)
 })
 
 //--- route qui ajoute un super héros
-app.post("/", existingHero, (req, res) => {
+app.post("/", existingHero, validateHero, (req, res) => {
     const newHero = {
-        id: superHeroes.length + 1,
+        slug: req.body.name.toLowerCase().replace(/[^\w]/gi,'-'),
         ...req.body
     }
 
     superHeroes = [...superHeroes, newHero]
-    res.json(superHeroes)
+    res.json(newHero)
 })
 
 //--- route qui ajoute un pouvoir à un super héros
-app.put("/:slug/power", (req, res) => {
+app.put("/:slug/power", successIfExists, (req, res) => {
     const { slug } = req.params
     const heroPower = superHeroes.find(power => power.slug === slug)
     const superpower = req.body.power // power = clé 'power' du fichier json
 
     heroPower.power = [...heroPower.power, superpower]
     res.json(heroPower.power)
+
+    //--- solution n°2 ---
+    // const { slug } = req.params
+    // const hero = superHeroes.find(hero => hero.slug === slug)
+    // hero.power = [ ...hero.power, req.body.power ] // => hero.power.push(req.body.power)
+    // res.json(hero)
 })
 
 //--- route qui efface un héros de la liste
-app.delete("/:slug", existingHero, (req, res) => {
+app.delete("/:slug", successIfExists, (req, res) => {
     const { slug } = req.params
-    const index = superHeroes.findIndex(hero => hero.slug === slug)
+    const hero = superHeroes.find(hero => hero.slug === slug)
 
-    superHeroes.splice(index, 1)
-    res.status(409).send(`The super hero ${index} has been delete.`)
+    //--- methode 1: splice ---
+    // const index = superHeroes.findIndex(hero => hero.slug === slug)
+    // superHeroes.splice(index, 1)
+
+    //--- methode 2: filter ---
+    superHeroes = superHeroes.filter(hero => hero.slug !== slug)
+
+    res.json(`The super hero ${hero.name} has been deleted.`)
 })
 
 //--- route qui efface un pouvoir du héro spécifique
-app.delete("/:slug/power/:power", existingHero, (req, res) => {
+app.delete("/:slug/power/:power", successIfExists, (req, res) => {
     const { slug, power } = req.params
     const hero = superHeroes.find(hero => hero.slug === slug)
-    const index = hero.power.findIndex(element => element === power )
 
-    hero.power.splice(index, 1)
-    res.status(409).send(`The power ${index} of the hero ${hero.name} has been delete.`)
+    //--- methode 1: splice ---
+    // const index = hero.power.findIndex(element => element === power )
+    // hero.power.splice(index, 1)
+
+    //--- methode 2: filter ---
+    hero.power = hero.power.filter(p => p !== power)
+
+    res.json(`The power ${power} of the hero ${hero.name} has been delete.`)
 })
 
 //--- route qui remplace tous les valeurs du héro existant par celles qui arrivent dans la requête
-app.put ("/:slug", existingHero, validateHero, (req,res) => {
+app.put ("/:slug", successIfExists, validateHero, (req,res) => {
     const { slug } = req.params
-    const indexHero = superHeroes.findIndex(element => element.slug === slug)
+    const indexHero = superHeroes.findIndex(hero => hero.slug === slug)
+    let hero = superHeroes[index]
 
-    superHeroes[indexHero].slug = req.body.slug
-    superHeroes[indexHero.name] = req.body.name
-    superHeroes[indexHero.power] = req.body.power
-    superHeroes[indexHero.color] = req.body.color
-    superHeroes[indexHero.isAlive] = req.body.isAlive
-    superHeroes[indexHero.age] = req.body.age
-    superHeroes[indexHero.image] = req.body.image
+    // res.json(heroes[indexHero])
 
-    res.json(heroes[indexHero])
+    hero = {
+        // hero de base
+        ...hero,
+    
+        // chaque clés de req.body dont le nom correspond a une clé du hero de base va mettre a jour la valeur de la clé du hero de base
+        ...req.body,
+        slug: req.body.name.toLowerCase().replace(/[^\w]/gi, '-')
+      }
+    
+
+    res.json(hero)
 })
 
 module.exports = app
